590a538212ca0f54953b55d6a790349bfa80afbe
Cullin <cullin_lam@student.uml.edu>
2016-10-01 00:30:15 -0400

    gettrack

--- httpfuncs.js
+++ httpfuncs.js
@@ -2,23 +2,38 @@ var Axios = require('axios')
 var test = "https://api.spotify.com/v1/search?q=Sorry&type=track&limit=1";
 var test2 = "https://api.spotify.com/v1/search";
 
-function getTrack(query){
+function getTrackTest(query){
     Axios.get(test).then(res=>{console.log(res.data.tracks.items[0])});
 }
 
-// function getTrack(query){
-//     Axios.get(test2,{
-//         params: {
-//             q:"Sorry",
-//             type:'track',
-//             limit:1
-//         }
-//     }).then(res => {
-//         console.log(res.data['']);
-//     })
+function getTrack(query){
+    Axios.get(test2,{
+        params: {
+            q:"Sorry",
+            type:'track',
+            limit:1
+        }
+    }).then(res => {
+        // console.log(console.log(res.data.tracks.items[0]));
+        var res_data = res.data.tracks.items[0];
+        var artist =  res_data.artists[0].name;
+        var track_name = res_data.name; 
+        var preview_url = res_data.preview_url;
+        var img_url = res_data.album.images[0].url;
+        console.log(artist);
+        console.log(preview_url);
+        console.log(img_url);
+        console.log(track_name);
+        return {
+            artist:artist,
+            track_name:track_name,
+            preview_url:preview_url,
+            img_url:img_url
+        }
+    })
 
-// }
+}
 
 getTrack(1);
-
+//getTrack2(2); 
 
