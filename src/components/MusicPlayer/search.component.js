1bacac75073950ea5ca227b4437b4ff7ccd16b2f
Huong Nguyen <huongnguyen@Huongs-MacBook-Pro.local>
2016-10-01 04:33:45 -0400

    search music implemented + lyrics css

--- src/components/MusicPlayer/search.component.js
+++ src/components/MusicPlayer/search.component.js
@@ -1,36 +1,37 @@
-import React from 'react';
-//import Autocomplete from 'react-autocomplete';
+import React, {Component} from 'react';
+// using Es6 we don't have to use const Component = React.Component;
 
-class Search extends React.Component{
-  handleRenderItem(item, isHighlighted){
-    const listStyles = {
-      item: {
-        padding: '2px 6px',
-        cursor: 'default'
-      },
 
-      highlightedItem: {
-        color: 'white',
-        background: '#F38B72',
-        padding: '2px 6px',
-        cursor: 'default'
-      }
-    };
-    return (
-      <div
-        style={isHighlighted ? listStyles.highlightedItem : listStyles.item}
-        key={item.id}
-        id={item.id}
-      >{item.title}</div>
-    )
+// State is a plain javascript object to record and react to user event.
+// Each class base Component has it own state object.
+// When ever a component state is change the component rerender and forces
+// all of its children as well.
+
+// Give SearchBar functionalities that belong to React
+class SearchBar extends Component{
+
+  constructor(props){
+    super(props); // going to throw an error if you don't call super
+
+    this.state = { term: '' };
   }
-  render() {
+
+  render(){
     return (
-      <div className="search">
-        
+      <div className="search-bar">
+        <input
+          value={this.state.term}
+          onChange={ (event) => this.onInputChange(event.target.value) } />
       </div>
     );
   }
+
+  onInputChange(term){
+    this.setState({term: term});
+
+    // Remember this is the from the parent component
+    this.props.onSearchTermChange(term)
+  }
 }
 
-export default Search
+export default SearchBar;
